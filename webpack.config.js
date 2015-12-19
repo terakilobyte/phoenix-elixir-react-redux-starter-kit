6e6ca4733cc886ecceabe5d72f3a7410b96af678
terakilobyte <terakilobyte@gmail.com>
2015-12-18 16:28:19 -0800

    Clean up webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  devtool: "source-map",
+  devtool: 'source-map',
   entry: {
-    "app": ["./web/static/css/app.scss", "./web/static/js/app.js"]
+    'app': ['./web/static/css/app.scss', './web/static/js/app.js']
   },
   output: {
-    path: "./priv/static",
-    filename: "/js/app.js"
+    path: './priv/static',
+    filename: '/js/app.js'
   },
   resolve: {
-    moduleDirectories: [__dirname + "/web/static/js"],
+    moduleDirectories: [__dirname + '/web/static/js'],
     alias: {
-      phoenix: __dirname + "/deps/phoenix/web/static/js/phoenix.js"
+      phoenix: __dirname + '/deps/phoenix/web/static/js/phoenix.js'
     }
   },
   module: {
     loaders: [{
       test: /\.jsx?$/,
       exclude: /node_modules/,
-      loader: "babel"
+      loader: 'babel'
     }, {
       test: /\.css$/,
-      loader: ExtractTextPlugin.extract("style", "css")
+      loader: ExtractTextPlugin.extract('style', 'css')
     }, {
       test: /\.scss$/,
       loader: ExtractTextPlugin.extract(
-        "style",
-        "css!sass?includePaths[]=" + __dirname + "/node_modules"
+        'style',
+        'css!sass?includePaths[]=' + __dirname + '/node_modules'
       )
     }, {
       test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'
@@ -41,11 +41,11 @@ module.exports = {
     }]
   },
   plugins: [
-    new ExtractTextPlugin("css/app.css"),
+    new ExtractTextPlugin('css/app.css'),
     new CopyWebpackPlugin([
-      { from: "./web/static/assets"},
-      { from: "./deps/phoenix_html/web/static/js/phoenix_html.js",
-        to: "js/phoenix_html.js"
+      { from: './web/static/assets'},
+      { from: './deps/phoenix_html/web/static/js/phoenix_html.js',
+        to: 'js/phoenix_html.js'
       }
     ])
   ]
